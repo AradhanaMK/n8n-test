e6b42d3d11d81a8fe77e8310d7b8f14a71c1aff9
Aradhana Mavanji Krishnamurthy <aradhana.mavanjikrishnamurthy@in.bosch.com>
2025-12-01 12:05:07 +0530

    Add python file

--- test.py
+++ test.py
@@ -1,15 +1,9 @@
-def find_largest_number(numbers):
-    if not numbers:
-        raise ValueError("The list is empty. Please provide a list of numbers.")
-    largest_number = numbers[0]  # Assume the first number is the largest initially
-    for current_number in numbers:
-        if current_number > largest_number:
-            largest_number = current_number
-    return largest_number
+numbers = [10, 25, 3, 99, 56, 78]
 
-# Example usage
-if __name__ == "__main__":
-    try:
-        print(find_largest_number([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]))  # Output should be 9
-    except ValueError as e:
-        print(e)
+largest = numbers[0]   # assume first number is largest
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest number:", largest)
